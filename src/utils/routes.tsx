b98fc9584c977a9836a6067650d399ca1cf11fba
Adeyanju Adeyemi <alex.olaolu007@gmail.com>
2023-03-30 19:18:53 +0100

    refactor: changed route hierarchy

--- src/utils/routes.tsx
+++ src/utils/routes.tsx
@@ -1,30 +1,34 @@
 import { Route } from "react-router-dom";
-import AppLayout from "../components/AppLayout";
-import Dashboard from "../pages/Dashboard";
-import ErrorPage from "../pages/ErrorPage";
-import MainPage from "../pages/MainPage";
+import AppLayout from "@/components/AppLayout";
+import Dashboard from "@/pages/Dashboard";
+import EditProductPage from "@/pages/EditProductPage";
+import ErrorPage from "@/pages/ErrorPage";
+import MainPage from "@/pages/MainPage";
+import ProductPage from "@/pages/ProductPage";
 
 
 export default function GetRoutes() {
    return (
       <Route
          path='/'
-         element={
-            <AppLayout>
-               <Dashboard />
-            </AppLayout>
-         }
+         element={<Dashboard />}
          errorElement={<ErrorPage />}
       >
          <Route
             index={true}
-            element={
-               <AppLayout>
-                  <MainPage />
-               </AppLayout>
-            }
+            element={<MainPage />}
             errorElement={<ErrorPage />}
-         ></Route>
+         />
+         <Route
+            path="product"
+            element={<ProductPage />}
+            errorElement={<ErrorPage />}
+         />
+         <Route
+            path="product/edit"
+            element={<EditProductPage />}
+            errorElement={<ErrorPage />}
+         />
 
       </Route>
    )
