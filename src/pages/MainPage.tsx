a212af9a60b9783336bd18b0b4ed9bb4166ef16e
Adeyanju Adeyemi <alex.olaolu007@gmail.com>
2023-03-30 19:16:50 +0100

    feat: created MainPage

---
+++ src/pages/MainPage.tsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+function MainPage() {
+    return (
+        <div className='text-center'>
+            THIS IS THE MAIN PAGE
+        </div>
+    )
+}
+
+export default MainPage
